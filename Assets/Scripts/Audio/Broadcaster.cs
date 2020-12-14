19515ea8e81381708112302cb5505f5b4007bb3a
Pedro Esteves <up201705160@fe.up.pt>
2020-12-14 19:36:10 +0000

    Added music

--- Assets/Scripts/Audio/Broadcaster.cs
+++ Assets/Scripts/Audio/Broadcaster.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Broadcaster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private string pathToMusics = null;
+    List<AudioClip> musics = new List<AudioClip>();
+
+    private float totalBroadcasterTime = 0f;
+
+    private int currentMusic = 0;
+
+    [SerializeField]
+    private Radio radio = null;
+
+    private void Start()
     {
-        
+        musics.AddRange(Resources.LoadAll<AudioClip>(pathToMusics));
+
+        totalBroadcasterTime = 0f;
+        foreach (AudioClip clip in musics)
+        {
+            totalBroadcasterTime += clip.length;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartBroadcaster(float time)
     {
-        
+        float currentTime = time % totalBroadcasterTime;
+
+        float tmp = 0;
+        for(int i = 0; i < musics.Count; i++)
+        {
+            if (tmp + musics[i].length > currentTime)
+            {
+                currentMusic = i;
+                radio.UpdateMusic(musics[i], currentTime - tmp);
+                StartCoroutine(NextMusic(currentTime - tmp));
+                break;
+            }
+            
+            tmp += musics[i].length;
+        }
+    }
+
+    public void StopBroadcaster()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator NextMusic(float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+
+            currentMusic = currentMusic + 1 % musics.Count;
+            radio.UpdateMusic(musics[currentMusic], 0);
+        }
     }
 }
--- Assets/Scripts/Audio/Radio.cs
+++ Assets/Scripts/Audio/Radio.cs
@@ -2,19 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Radio : MonoBehaviour
 {
-    List<Broadcaster> broadcasters;
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private List<Broadcaster> broadcasters = new List<Broadcaster>();
+    
+    private int currentBroadcaster = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+
+        broadcasters[0].StartBroadcaster(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpdateMusic(AudioClip clip, float time)
+    {
+        audioSource.clip = clip;
+        audioSource.time = time;
+        audioSource.Play();
     }
 }
