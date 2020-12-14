38d7c5f8dcd93ff728c1a30e463f5c1c5750f8eb
Pedro Esteves <up201705160@fe.up.pt>
2020-12-14 16:09:40 +0000

    Started PlayerMovement script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator = null;
+    [SerializeField]
+    private Rigidbody2D rb = null;
+    [SerializeField]
+    private SpriteRenderer sr = null;
+
+    [SerializeField]
+    private float jumpForce = 5f, speed = .2f;
+
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            float horDir = Input.GetAxisRaw("Horizontal");
+
+            if (horDir < 0)
+            {
+                sr.flipX = true;
+            }
+            else
+            {
+                sr.flipX = false;
+            }
+
+
+            transform.Translate(Vector2.right * horDir * speed, Space.World);
+
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {       
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            SetJumping(true);
+        }
+    }
+
+    private void SetJumping(bool jump)
+    {
+        isJumping = jump;
+        animator.SetBool("jumping", jump);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            SetJumping(false);
+        }
+    }
+}
