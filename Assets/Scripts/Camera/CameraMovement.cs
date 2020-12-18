870a835a63b9c62b010240981e2a1dcf529d47bd
Pedro Esteves <up201705160@fe.up.pt>
2020-12-18 23:18:07 +0000

    Added CameraMovement script. Moved other scripts to folders.

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player = null;
+
+    [SerializeField]
+    private float minX = -15f, maxX = 15f; //Test values -> must be changed according to the level
+
+    private float cameraYPos, cameraZPos;
+    private float halfScreenWidth;
+
+    private void Start()
+    {
+        cameraYPos = transform.position.y;
+        cameraZPos = transform.position.z;
+
+        Vector2 edgeVector = Camera.main.ViewportToWorldPoint(Vector2.one);
+        halfScreenWidth = edgeVector.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.x - halfScreenWidth > minX && player.transform.position.x + halfScreenWidth < maxX)
+        {
+            transform.position = new Vector3(player.transform.position.x, cameraYPos, cameraZPos);
+        }
+    }
+}
