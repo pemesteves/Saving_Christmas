a8eef3d99219f9b8799dff209f979153373ee597
Pedro Esteves <up201705160@fe.up.pt>
2020-12-19 18:41:21 +0000

    Added reindeer. Player can know pick a reideer and the reindeer will be added to the sled

---
+++ Assets/Scripts/Sled/Reindeer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Reindeer : MonoBehaviour
+{
+    [SerializeField]
+    private Sled sled = null;
+
+    private bool collidingWithPlayer = false;
+    private bool pickedReindeer = false;
+
+    [SerializeField]
+    private GameObject canvasBalloon = null;
+
+    [SerializeField]
+    private TMP_Text helpText = null, thankYouText = null;
+
+    private void Update()
+    {
+        if (!collidingWithPlayer || pickedReindeer) return;
+
+        if(Input.GetButtonDown("PickItem"))
+        {
+            pickedReindeer = true;
+            sled.PickedReindeer();
+
+            helpText?.gameObject.SetActive(false);
+            thankYouText?.gameObject.SetActive(true);
+
+
+            StartCoroutine(FadeReindeer());
+        }
+    }
+
+    private IEnumerator FadeReindeer()
+    {
+        SpriteRenderer sp = GetComponent<SpriteRenderer>();
+
+        for(float t = 0; t <= 1f; t += Time.deltaTime)
+        {
+            sp.color = new Color(sp.color.r, sp.color.g, sp.color.b, Mathf.Lerp(1, 0, t));
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canvasBalloon?.SetActive(true);
+        CheckCollisionWithPlayer(collision, true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canvasBalloon?.SetActive(false);
+        CheckCollisionWithPlayer(collision, false);
+    }
+
+    private void CheckCollisionWithPlayer(Collider2D collision, bool colliding)
+    {
+        if (collision.CompareTag("Player")) collidingWithPlayer = colliding;
+    }
+}
---
+++ Assets/Scripts/Sled/Sled.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+struct ReindeerValues
+{
+    public Vector2 reindeerPosition;
+    public float ropeLength;
+    public int orderInLayer;
+}
+
+public class Sled : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject reindeer = null;
+
+    [SerializeField]
+    private List<ReindeerValues> reindeerValues = new List<ReindeerValues>();
+
+    [SerializeField]
+    private GameObject rope = null;
+
+    private int numReindeers = 0;
+
+    // OnTriggerEnter2D (collision with player)
+
+    public void PickedReindeer()
+    {
+        if (numReindeers >= reindeerValues.Count) return;
+
+        ReindeerValues values = reindeerValues[numReindeers];
+
+        GameObject deer = Instantiate(reindeer, transform);
+        deer.transform.localPosition = values.reindeerPosition;
+        SpriteRenderer sp = deer.GetComponent<SpriteRenderer>();
+        sp.sortingOrder = values.orderInLayer;
+
+        rope.transform.localScale = new Vector3(values.ropeLength, rope.transform.localScale.y, rope.transform.localScale.z);
+
+        numReindeers++;
+    }
+}
