e90e08fa7b3240780626b9eb34eb19833a6c3520
Pedro Esteves <up201705160@fe.up.pt>
2020-12-19 21:19:11 +0000

    Started Menu scene. Added GameController and MenuController

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menuCanvas = null;
+    [SerializeField]
+    private GameObject instructionsCanvas = null;
+    [SerializeField]
+    private GameObject creditCanvas = null;
+
+    public void ShowMenu(string name)
+    {
+        switch(name)
+        {
+            case "Menu":
+                SetMenusActive(true, false, false);
+                break;
+            case "Instructions":
+                SetMenusActive(false, true, false);
+                break;
+            case "Credits":
+                SetMenusActive(false, false, true);
+                break;
+            default:
+                break;
+        }        
+    }
+
+    private void SetMenusActive(bool menu, bool instructions, bool credits)
+    {
+        menuCanvas.SetActive(menu);
+        instructionsCanvas.SetActive(instructions);
+        creditCanvas.SetActive(credits);
+    }
+}
