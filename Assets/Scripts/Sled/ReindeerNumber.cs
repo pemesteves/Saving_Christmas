875edda7594d52aa048c675499995a5edccbf8c8
Pedro Esteves <up201705160@fe.up.pt>
2020-12-19 19:28:56 +0000

    Improved sled. Added reindeer number to the UI

---
+++ Assets/Scripts/Sled/ReindeerNumber.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ReindeerNumber : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text textNum = null;
+
+    public void SetNumReindeers(int num)
+    {
+        textNum.text = num.ToString();
+    }
+}
--- Assets/Scripts/Sled/Sled.cs
+++ Assets/Scripts/Sled/Sled.cs
@@ -21,6 +21,16 @@ public class Sled : MonoBehaviour
     [SerializeField]
     private GameObject rope = null;
 
+    [SerializeField]
+    private ReindeerNumber reindeerNumber = null;
+
+    [SerializeField]
+    private GameObject canvasBalloon = null;
+    [SerializeField]
+    private GameObject helpText = null, nextLevelText = null;
+
+    private bool collidingWithPlayer = false;
+
     private int numReindeers = 0;
 
     // OnTriggerEnter2D (collision with player)
@@ -39,5 +49,30 @@ public class Sled : MonoBehaviour
         rope.transform.localScale = new Vector3(values.ropeLength, rope.transform.localScale.y, rope.transform.localScale.z);
 
         numReindeers++;
+
+        if(numReindeers == reindeerValues.Count)
+        {
+            helpText?.SetActive(false);
+            nextLevelText?.SetActive(true);
+        }
+
+        reindeerNumber.SetNumReindeers(reindeerValues.Count - numReindeers);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canvasBalloon?.SetActive(true);
+        CheckCollisionWithPlayer(collision, true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canvasBalloon?.SetActive(false);
+        CheckCollisionWithPlayer(collision, false);
+    }
+
+    private void CheckCollisionWithPlayer(Collider2D collision, bool colliding)
+    {
+        if (collision.CompareTag("Player")) collidingWithPlayer = colliding;
     }
 }
