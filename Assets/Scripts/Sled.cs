2f50820e3c2a23e262cfd088ec366e688f2f6867
Pedro Esteves <up201705160@fe.up.pt>
2020-12-19 11:51:27 +0000

    Added Sled script. Changed ground grid

---
+++ Assets/Scripts/Sled.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+struct ReindeerValues
+{
+    public Vector2 reindeerPosition;
+    public float ropeLength;
+    public int orderInLayer;
+}
+
+public class Sled : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject reindeer = null;
+
+    [SerializeField]
+    private List<ReindeerValues> reindeerValues = new List<ReindeerValues>();
+
+    // OnTriggerEnter2D (collision with player)
+    // Functions to instantiate reindeers -> track the number of reindeers
+}
