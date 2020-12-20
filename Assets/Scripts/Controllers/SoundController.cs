dd9f18c701ded5060da1c1b644371c243cdb4f48
Pedro Esteves <up201705160@fe.up.pt>
2020-12-20 19:57:46 +0000

    Fixed sound options

--- Assets/Scripts/Controllers/SoundController.cs
+++ Assets/Scripts/Controllers/SoundController.cs
@@ -1,23 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundController : MonoBehaviour
 {
     [SerializeField]
     private Slider slider = null;
-    [SerializeField]
-    private AudioSourceManager audioSourceManager = null;
+    private List<AudioSourceManager> audioSourceManagers = new List<AudioSourceManager>();
 
     // Start is called before the first frame update
     void Start()
     {
         slider.value = PlayerPrefs.GetFloat("SoundVolume", .5f);
+
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("AudioSourceManager");
+        foreach(GameObject obj in objs)
+        {
+            audioSourceManagers.Add(obj.GetComponent<AudioSourceManager>());
+        }
     }
 
     public void ChangedValue(float value)
     {
         slider.value = value;
         PlayerPrefs.SetFloat("SoundVolume", value);
-        audioSourceManager.UpdateAudioSourceVolume();
+        foreach (AudioSourceManager audioSourceManager in audioSourceManagers)
+        {
+            audioSourceManager.UpdateAudioSourceVolume();
+        }
     }
 }
